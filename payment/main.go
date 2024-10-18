be9529f44235d3e6670cf116c2df7794e98a7f06
JoaoFelipe007 <joaofelipedasilva687@gmail.com>
2024-10-17 21:51:19 -0300

    refac: Colocando os nomes certos

--- payment/main.go
+++ payment/main.go
@@ -1,59 +1,65 @@
 package main
 
 import (
- "encoding/json"
- "log"
- "net/http"
+	"encoding/json"
+	"log"
+	"net/http"
 )
-type Tarefa struct {
- ID         int    `json:"id"`
- Descricao  string `json:"descricao"`
+
+type Pagamento struct {
+	ID        int    `json:"id"`
+	Descricao string `json:"descricao"`
 }
-var tarefas []Tarefa
+
+var pagamentos = []Pagamento{
+	{ID: 1, Descricao: "TESTE 1"},
+	{ID: 2, Descricao: "TESTE"},
+}
+
 func main() {
- http.HandleFunc("/", handleRoot)
- http.HandleFunc("/tarefas", handleTarefas)
- log.Fatal(http.ListenAndServe(":8081", nil))
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/payment", handlePagamentos)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 func handleRoot(w http.ResponseWriter, r *http.Request) {
- w.WriteHeader(http.StatusOK)
- w.Write([]byte("Bem-vindo à API de tarefas!"))
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Bem-vindo à API de pagamentos!"))
 }
-func handleTarefas(w http.ResponseWriter, r *http.Request) {
- switch r.Method {
- case http.MethodGet:
-  listarTarefas(w, r)
- case http.MethodPost:
-  criarTarefa(w, r)
- case http.MethodPut:
-  atualizarTarefa(w, r)
- case http.MethodDelete:
-  excluirTarefa(w, r)
- default:
-  w.WriteHeader(http.StatusMethodNotAllowed)
-  w.Write([]byte("Método não permitido"))
- }
+func handlePagamentos(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		listarPagamentos(w, r)
+	case http.MethodPost:
+		criarPagamento(w, r)
+	case http.MethodPut:
+		atualizarPagamento(w, r)
+	case http.MethodDelete:
+		excluirPagamento(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Método não permitido"))
+	}
 }
-func listarTarefas(w http.ResponseWriter, r *http.Request) {
- w.Header().Set("Content-Type", "application/json")
- json.NewEncoder(w).Encode(tarefas)
+func listarPagamentos(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(pagamentos)
 }
-func criarTarefa(w http.ResponseWriter, r *http.Request) {
- var tarefa Tarefa
- err := json.NewDecoder(r.Body).Decode(&tarefa)
- if err != nil {
-  w.WriteHeader(http.StatusBadRequest)
-  return
- }
- tarefa.ID = len(tarefas) + 1
- tarefas = append(tarefas, tarefa)
- w.Header().Set("Content-Type", "application/json")
- w.WriteHeader(http.StatusCreated)
- json.NewEncoder(w).Encode(tarefa)
+func criarPagamento(w http.ResponseWriter, r *http.Request) {
+	var pagamento Pagamento
+	err := json.NewDecoder(r.Body).Decode(&pagamento)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	pagamento.ID = len(pagamentos) + 1
+	//	pagamento = append(pagamentos, pagamento)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(pagamento)
 }
-func atualizarTarefa(w http.ResponseWriter, r *http.Request) {
- // Implemente a lógica de atualização aqui
+func atualizarPagamento(w http.ResponseWriter, r *http.Request) {
+	// Implemente a lógica de atualização aqui
 }
-func excluirTarefa(w http.ResponseWriter, r *http.Request) {
- // Implemente a lógica de exclusão aqui
+func excluirPagamento(w http.ResponseWriter, r *http.Request) {
+	// Implemente a lógica de exclusão aqui
 }
