6621ed160f4491a0ac8575142a50ac6a217d86b7
Diogo Shikuma <90211130+diogoshkm@users.noreply.github.com>
2024-11-22 00:24:22 +0000

    correção dos testes

--- estoque/estoque.js
+++ estoque/estoque.js
@@ -3,10 +3,12 @@ const app = express()
 const port = 3000
 
 // Rotas
-app.get('/health/estoque', (req, res) => {
-  res.json({status: ok}).status(200)
+app.get('/estoque/health', (req, res) => {
+    res.json({status: 'ok'}).status(200)
 })
 
 app.listen(port, () => {
   console.log(`Aplicação iniciada e ouvindo na porta: ${port}`)
 })
+
+module.exports = app
--- estoque/estoque.test.js
+++ estoque/estoque.test.js
@@ -1,14 +1,14 @@
 const request = require('supertest');
-const app = require('../estoque'); // Substitua pelo caminho correto para o seu app
+const app = require('./estoque'); // Substitua pelo caminho correto para o seu app
 
-describe('GET /health/estoque', () => {
+describe('GET /estoque/health', () => {
   it('should respond with status 200', async () => {
-    const response = await request(app).get('/health/estoque');
+    const response = await request(app).get('/estoque/health');
     expect(response.status).toBe(200);
   });
 
   it('should respond with the correct message', async () => {
-    const response = await request(app).get('/health/estoque');
+    const response = await request(app).get('/estoque/health');
     expect(response.body).toEqual({ status: 'ok' }); // Ajuste conforme a resposta esperada
   });
 });
